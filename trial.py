ed293dda957ab32a34a176f208dd0f55313762ee
michalkoziarski <michalkoziarski@gmail.com>
2018-02-23 17:26:58 +0100

    extended argparse arguments

--- trial.py
+++ trial.py
@@ -8,21 +8,24 @@ import pandas as pd
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-dataset', choices=datasets.names(), required=True)
-parser.add_argument('-partition', choices=[1, 2, 3, 4, 5], required=True, type=int)
-parser.add_argument('-fold', choices=[1, 2], required=True, type=int)
+parser.add_argument('-partition', choices=[1, 2, 3, 4, 5], required=True)
+parser.add_argument('-fold', choices=[1, 2], required=True)
+parser.add_argument('-mode', choices=['OVA', 'OVO'], required=True, default='OVA')
+parser.add_argument('-method', choices=['sampling', 'complete'], required=True, default='sampling')
+parser.add_argument('-results_path', type=str, required=True,
+                    default=os.path.join(os.path.dirname(__file__), 'results'))
 
 args = parser.parse_args()
 
-algorithm = algorithms.MultiClassRBO()
+algorithm = algorithms.MultiClassRBO(method=args.method)
 
 (X_train, y_train), (X_test, y_test) = datasets.load(args.dataset, args.partition, args.fold)
 
 X_train, y_train = algorithm.fit_sample(X_train, y_train)
 
-results_path = os.path.join(os.path.dirname(__file__), 'results')
-dataset_path = os.path.join(results_path, args.dataset)
+dataset_path = os.path.join(args.results_path, args.dataset)
 
-for path in [results_path, dataset_path]:
+for path in [args.results_path, dataset_path]:
     if not os.path.exists(path):
         os.mkdir(path)
 

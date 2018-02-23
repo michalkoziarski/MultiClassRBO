9a41d03ddfcc128a02bf2b9b04c6e7a0f088d3c0
michalkoziarski <michalkoziarski@gmail.com>
2018-02-23 20:07:58 +0100

    trial runner updated for different modes and methods

--- trial.py
+++ trial.py
@@ -8,20 +8,13 @@ import pandas as pd
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-dataset', choices=datasets.names(), required=True)
-parser.add_argument('-partition', choices=[1, 2, 3, 4, 5], required=True)
-parser.add_argument('-fold', choices=[1, 2], required=True)
+parser.add_argument('-partition', choices=[1, 2, 3, 4, 5], type=int, required=True)
+parser.add_argument('-fold', choices=[1, 2], type=int, required=True)
 parser.add_argument('-mode', choices=['OVA', 'OVO'], required=True, default='OVA')
-parser.add_argument('-method', choices=['sampling', 'complete'], required=True, default='sampling')
-parser.add_argument('-results_path', type=str, required=True,
-                    default=os.path.join(os.path.dirname(__file__), 'results'))
+parser.add_argument('-method', choices=['sampling', 'complete'], default='sampling')
+parser.add_argument('-results_path', type=str, default=os.path.join(os.path.dirname(__file__), 'results'))
 
 args = parser.parse_args()
-
-algorithm = algorithms.MultiClassRBO(method=args.method)
-
-(X_train, y_train), (X_test, y_test) = datasets.load(args.dataset, args.partition, args.fold)
-
-X_train, y_train = algorithm.fit_sample(X_train, y_train)
 
 dataset_path = os.path.join(args.results_path, args.dataset)
 
@@ -29,9 +22,47 @@ for path in [args.results_path, dataset_path]:
     if not os.path.exists(path):
         os.mkdir(path)
 
-output_file_name = '%s.%d.%d.train.oversampled.csv' % (args.dataset, args.partition, args.fold)
-output_path = os.path.join(dataset_path, output_file_name)
-original_file_name = output_file_name.replace('.oversampled', '')
-original_path = os.path.join(os.path.join(os.path.dirname(__file__), 'data', 'folds', args.dataset, original_file_name))
-header = pd.read_csv(original_path).columns
-pd.DataFrame(np.c_[X_train, y_train]).to_csv(output_path, index=False, header=header)
+(X_train, y_train), (X_test, y_test) = datasets.load(args.dataset, args.partition, args.fold)
+
+header = pd.read_csv(
+    os.path.join(
+        os.path.dirname(__file__),
+        'data',
+        'folds',
+        args.dataset,
+        '%s.%d.%d.train.csv' % (args.dataset, args.partition, args.fold)
+    )
+).columns
+
+if args.mode == 'OVA':
+    X_train, y_train = algorithms.MultiClassRBO(method=args.method).fit_sample(X_train, y_train)
+
+    output_path = os.path.join(dataset_path, '%s.%d.%d.train.oversampled.csv' % (
+        args.dataset, args.partition, args.fold
+    ))
+
+    pd.DataFrame(np.c_[X_train, y_train]).to_csv(output_path, index=False, header=header)
+elif args.mode == 'OVO':
+    classes = np.unique(np.concatenate([y_train, y_test]))
+
+    for i in range(len(classes)):
+        for j in range(i + 1, len(classes)):
+            indices = ((y_train == classes[i]) | (y_train == classes[j]))
+
+            X, y = X_train[indices].copy(), y_train[indices].copy()
+
+            class_distribution = {cls: len(y[y == cls]) for cls in [classes[i], classes[j]]}
+            minority_class = min(class_distribution, key=class_distribution.get)
+
+            appended = algorithms.RBO().fit_sample(X, y)
+
+            if len(appended) > 0:
+                X, y = np.concatenate([X, appended]), np.concatenate([y, np.tile([minority_class], len(appended))])
+
+            output_path = os.path.join(dataset_path, '%s.%d.%d.train.oversampled.%dv%d.csv' % (
+                args.dataset, args.partition, args.fold, classes[i], classes[j]
+            ))
+
+            pd.DataFrame(np.c_[X, y]).to_csv(output_path, index=False, header=header)
+else:
+    raise NotImplementedError
